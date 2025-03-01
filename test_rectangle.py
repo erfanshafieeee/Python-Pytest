d3186f534266145908f7cc87c11807d746ee0040
erfan <erfanshafieeee@gmail.com>
2025-03-01 14:38:25 +0330

    fixture

---
+++ test_rectangle.py
@@ -0,0 +1,15 @@
+import pytest
+import Shapes as shapes
+
+@pytest.fixture
+def my_rectangle():
+    return shapes.Rectangle(10 , 20)
+
+def test_area(my_rectangle):
+    
+    assert my_rectangle.area() == 10 * 20 
+
+def test_perimeter(my_rectangle):
+    
+    assert my_rectangle.perimeter() == ((10 * 2 ) + (20 *2))
+
