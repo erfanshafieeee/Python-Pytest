545fc96b5810686110b6683d2ef4496e66896658
erfan <erfanshafieeee@gmail.com>
2025-03-01 14:56:43 +0330

    copy fixtures in conftest file

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+import Shapes as shapes
+@pytest.fixture
+def my_rectangle():
+    return shapes.Rectangle(10 , 20)
+
+@pytest.fixture
+def weird_rectangle():
+    return shapes.Rectangle(5 , 6)
--- test_rectangle.py
+++ test_rectangle.py
@@ -1,20 +1,10 @@
 import pytest
 import Shapes as shapes
 
-@pytest.fixture
-def my_rectangle():
-    return shapes.Rectangle(10 , 20)
-
-@pytest.fixture
-def weird_rectangle():
-    return shapes.Rectangle(5 , 6)
-
 def test_area(my_rectangle):
-    
     assert my_rectangle.area() == 10 * 20 
 
 def test_perimeter(my_rectangle):
-    
     assert my_rectangle.perimeter() == ((10 * 2 ) + (20 *2))
 
 
